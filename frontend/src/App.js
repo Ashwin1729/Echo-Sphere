c621b307b46001a0e821afe0ecd2e2a9ca1f242e
Ashwin1729 <mathematicswroteus@gmail.com>
2023-07-24 02:18:30 +0530

    authentication page added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,11 @@ import React from "react";
 import HomePage from "./pages/HomePage";
 import { Route } from "react-router-dom";
 import ChatPage from "./pages/ChatPage";
+import "./App.css";
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Route path="/" component={HomePage} exact />
       <Route path="/chats" component={ChatPage} exact />
     </div>
--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,7 +1,61 @@
-import React, { useEffect } from "react";
+import React from "react";
+import {
+  Box,
+  Container,
+  Tab,
+  TabList,
+  TabPanel,
+  TabPanels,
+  Tabs,
+  Center,
+  TabIndicator,
+} from "@chakra-ui/react";
+import Login from "../components/authentication/Login";
+import SignUp from "../components/authentication/SignUp";
 
 const HomePage = () => {
-  return <div>Hello World!</div>;
+  return (
+    <Container maxW={"xl"} centerContent>
+      <Box
+        d="flex"
+        justifyContent="flex-end"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+      >
+        <Center fontSize="2xl" fontFamily="Work sans">
+          Echo Sphere
+        </Center>
+      </Box>
+      <Box bg="white" w="100%" p={4} borderRadius="lg" borderWidth="1px">
+        <Tabs isFitted variant="unstyled">
+          <TabList mb="10px">
+            <Tab borderRight="1px" borderColor="gray.300">
+              Login
+            </Tab>
+            <Tab>Sign Up</Tab>
+          </TabList>
+          <TabIndicator
+            mt="-1px"
+            height="2px"
+            bg="blue.500"
+            borderRadius="1px"
+          />
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
 };
 
 export default HomePage;
